d6ca66032875650bce05b671baa92288efda57cf
Sebastien Grosjean <public@zencocoon.com>
2009-11-07 16:14:18 +0200

    Improve Date.fromJSON by only generating timestamp and not new Date

--- src/prototype-date.js
+++ src/prototype-date.js
@@ -12,19 +12,14 @@
 *
 * <h5>Example</h5>
 *
-* new Date.fromJSON("2009-11-06T13:30:12Z");
+* Date.fromJSON("2009-11-06T13:30:12Z");
+* //-> 1257514212000
+* new Date(Date.fromJSON("2009-11-06T13:30:12Z"));
 * //-> Fri Nov 06 2009 15:30:12 GMT+0200 (EET)
 **/
 Date.fromJSON = function(string) {
   var match = string.match(new RegExp(/(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z/));
 
-  var date = new Date();
-  date.setUTCFullYear(parseInt(match[1], 10));
-  date.setUTCMonth(parseInt(match[2], 10) - 1);
-  date.setUTCDate(parseInt(match[3], 10));
-  date.setUTCHours(parseInt(match[4], 10));
-  date.setUTCMinutes(parseInt(match[5], 10));
-  date.setUTCSeconds(parseInt(match[6], 10));
-  
-  return date;
+  return(Date.UTC(parseInt(match[1], 10), parseInt(match[2], 10) - 1, parseInt(match[3], 10),
+    parseInt(match[4], 10), parseInt(match[5], 10), parseInt(match[6], 10)));
 };
